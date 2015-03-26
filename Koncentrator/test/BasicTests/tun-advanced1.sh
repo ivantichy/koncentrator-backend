9c60316101306bb7857c34437a5f890cb05b00c0
ivan <ivan.tichy@gmail.com>
2015-03-26 20:41:55 +0100

    ./Koncentrator/test/BasicTests/

--- Koncentrator/test/BasicTests/tun-advanced1.sh
+++ Koncentrator/test/BasicTests/tun-advanced1.sh
@@ -6,5 +6,5 @@ java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.httpapi.handler
 
 sleep 10
 
-curl -v -f -X PUT 127.0.0.1:10001/test -d @Koncentrator/target/classes/cz/ivantichy/httpapi/handlers/vpnapi/test.json
+curl -v -f -X PUT 127.0.0.1:10001/test -d @./Koncentrator/test/BasicTests/test.json
 
