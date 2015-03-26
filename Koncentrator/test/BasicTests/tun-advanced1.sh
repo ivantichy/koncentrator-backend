fe78312e88a08d4e61494faabba3b130fa3c852a
ivan <ivan.tichy@gmail.com>
2015-03-26 20:34:17 +0100

    fixed test

--- Koncentrator/test/BasicTests/tun-advanced1.sh
+++ Koncentrator/test/BasicTests/tun-advanced1.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-java -classpath ./Koncentrator/*;./Koncentrator/lib/* cz.ivantichy.httpapi.handlers.vpnapi.CreateSubVPNAdapterTest &
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.httpapi.handlers.vpnapi.CreateSubVPNAdapterTest &
+
+sleep 10
 
 curl -v -f -X PUT 127.0.0.1:10001/test -d @Koncentrator/target/classes/cz/ivantichy/httpapi/handlers/vpnapi/test.json
 
