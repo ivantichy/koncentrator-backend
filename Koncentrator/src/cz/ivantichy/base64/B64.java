3cbbf92f686d144e8cb053154ec7326e246754ff
ivan <ivan.tichy@gmail.com>
2015-03-30 11:16:16 +0200

    autocommit

--- Koncentrator/src/cz/ivantichy/base64/B64.java
+++ Koncentrator/src/cz/ivantichy/base64/B64.java
@@ -7,7 +7,7 @@ public class B64 {
 	public static String encode(String string) {
 
 		return new String(Base64.getEncoder().encode(string.getBytes()))
-				.replaceAll("\\w", "");
+				.replaceAll("\\s", "");
 	}
 
 	public static String decode(String string) {
