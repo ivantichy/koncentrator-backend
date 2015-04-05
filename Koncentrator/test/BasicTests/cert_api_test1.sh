6e175989d257e415c4b643cc06db80d381f66246
ivan <ivan.tichy@gmail.com>
2015-04-05 13:47:30 +0200

    autocommit

--- Koncentrator/test/BasicTests/cert_api_test1.sh
+++ Koncentrator/test/BasicTests/cert_api_test1.sh
@@ -5,8 +5,14 @@ set -o pipefail
 
 rm -R -f /etc/openvpn/easy-rsa/2.0/instances/*
 
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.CreateCaAdapterTest | tee /tmp/ca
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateServerAdapterTest | tee /tmp/server
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest | tee /tmp/profile
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.DeleteCaAdapterTest | tee /tmp/delete
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.CreateCaAdapterTest | tee ca
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateServerAdapterTest | tee server
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest | tee profile
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.DeleteCaAdapterTest | tee delete
+
+chown jenkins:jenkins ca
+chown jenkins:jenkins server
+chown jenkins:jenkins profile
+chown jenkins:jenkins delete
+
 
