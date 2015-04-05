5a404790bee701c6e3972b3e3ddd954872b453db
ivan <ivan.tichy@gmail.com>
2015-04-05 20:43:44 +0200

    autocommit

--- Koncentrator/test/BasicTests/cleanserver.sh
+++ Koncentrator/test/BasicTests/cleanserver.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 
-sudo -i -u java << EOF
-
 cd $WORKSPACE/Koncentrator/FS/VPN
 cp -v -r -f * /
 cd /etc/openvpn
@@ -10,4 +8,3 @@ find . -type f | grep .sh | xargs  -n1 chmod +x $1 -v
 
 sudo iptables-restore /etc/iptables/rules.v4.backup
 
-EOF
