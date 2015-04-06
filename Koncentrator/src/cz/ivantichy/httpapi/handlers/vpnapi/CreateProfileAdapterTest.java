6600dbf7803ad8c38ddc189f7376f3877dceca19
ivan <ivan.tichy@gmail.com>
2015-04-06 12:50:26 +0200

    autocommit

---
+++ Koncentrator/src/cz/ivantichy/httpapi/handlers/vpnapi/CreateProfileAdapterTest.java
@@ -0,0 +1,24 @@
+package cz.ivantichy.httpapi.handlers.vpnapi;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import cz.ivantichy.supersimple.restapi.server.PUTRequest;
+import cz.ivantichy.supersimple.restapi.server.Server;
+
+import cz.ivantichy.fileutils.*;
+
+public class CreateProfileAdapterTest {
+
+	@Test()
+	public static void main(String[] args) throws IOException {
+
+		CreateProfileAdapter cpa = new CreateProfileAdapter();
+
+		String json = FileWork.readFile(args[0]);
+
+		cpa.handlePUT(new PUTRequest(null, null, null, json, null));
+
+	}
+}
