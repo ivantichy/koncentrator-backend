f3b550b547df9a9c8c7193c8c46fb5c4ab5826ad
ivan <ivan.tichy@gmail.com>
2015-04-07 21:41:46 +0200

    autocommit

--- Koncentrator/test/BasicTests/new_tun_basic_test.sh
+++ Koncentrator/test/BasicTests/new_tun_basic_test.sh
@@ -22,7 +22,7 @@ wget "http://127.0.0.1:10001/generateserver/?subvpn_name=tun-basic-12345&subvpn_
 #CERT generateProfile
 wget "http://127.0.0.1:10001/generateprofile/?subvpn_name=tun-basic-12345&subvpn_type=tun-basic&common_name=test1&domain=koncentrator.cz&profile_valid_days=90" -O profile.json -T 10
 
-
+chmod 666 *.json
 
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json ip_server 123.123.123.123
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json ip_mask 255.255.0.0
