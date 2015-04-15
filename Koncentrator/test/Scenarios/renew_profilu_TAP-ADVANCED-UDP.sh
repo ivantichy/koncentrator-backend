9f7859a413e29eeac02ed3b59ae0c1384a4f7692
ivan <ivan.tichy@gmail.com>
2015-04-15 13:58:18 +0200

    autocommit

---
+++ Koncentrator/test/Scenarios/renew_profilu_TAP-ADVANCED-UDP.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex 
+set -o pipefail
+
+set +e
+pkill -e -f ".*cz.ivantichy.httpapi.handlers.vpnapi.*"
+set -e
+
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.httpapi.handlers.vpnapi.RunnerVPN &
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.httpapi.handlers.vpnapi.RunnerCERT &
+
+sleep 5
+
+
+#CERT generateProfile
+wget "http://127.0.0.1:10001/generateprofile/?subvpn_name=tap-advanced-12345&subvpn_type=tap-advanced&common_name=test-hash-1111&domain=tap-advanced-12345.tap-advanced.koncentrator.cz&profile_valid_days=180" -O profile.json -T 10
+
+chmod 666 *.json
+
+./zablokovani_profilu_TAP-ADVANCED-UDP.sh
+./smazani_profilu_TAP-ADVANCED-UDP.sh
+# pokud nedoslo ke zmene IP adresy (pokud ano -> wait)
+./vytvoreni_profilu_TAP-ADVANCED-UDP.sh
+
+pkill -e -f ".*cz.ivantichy.httpapi.handlers.vpnapi.*"
