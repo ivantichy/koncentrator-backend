19e332fd95f9ae114a9e52bf83b9034e8b95a1e7
ivan <ivan.tichy@gmail.com>
2015-05-03 21:34:53 +0200

    autocommit

--- Koncentrator/test/BasicTests/new_tun_basic_test.sh
+++ Koncentrator/test/BasicTests/new_tun_basic_test.sh
@@ -13,51 +13,29 @@ java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.httpapi.handler
 sleep 5
 
 
-#CERT createCa 
-curl -v -X PUT "http://127.0.0.1:10001/createca" --data '{"subvpn_name" : "tap-advanced-12345", "subvpn_type" : "tap-advanced", "domain" : "tap-advanced-12345.tap-advanced.koncentrator.cz", "ca_valid_days" : 3650}' -o ca.json
-
-#CERT generateServer
-wget "http://127.0.0.1:10001/generateserver/?subvpn_name=tap-advanced-12345&subvpn_type=tap-advanced&common_name=tap-advanced-12345&domain=koncentrator.cz&server_valid_days=3650" -O server.json
-
 #CERT generateProfile
-wget "http://127.0.0.1:10001/generateprofile/?subvpn_name=tap-advanced-12345&subvpn_type=tap-advanced&common_name=test1&domain=koncentrator.cz&profile_valid_days=90" -O profile.json -T 10
+wget "http://127.0.0.1:10001/generateprofile/?subvpn_name=tun-basic-12345&subvpn_type=tun-basic&common_name=test1&domain=koncentrator.cz&profile_valid_days=90" -O profile.json -T 10
 
 chmod 666 *.json
 
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json ip_server 123.123.123.123
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json ip_mask 255.255.0.0
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json node 1
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json server_device tun1
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json server_management_port 20001
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json server_protocol udp
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter ca.json server_domain_name tun-advanced.koncentrator.cz
 
-
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter server.json server_commands ""
 
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter profile.json remote_ip 10.10.10.10
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter profile.json local_ip 10.10.10.11
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.support.JSON.test.JSONAddParameter profile.json profile_commands ""
 
 
-
-#VPN createCa 
-curl -v -X PUT "http://digitalocean.ivantichy.cz:10002/createsubvpn" -d @ca.json
-
-#VPN createServer
-curl -v -X PUT "http://digitalocean.ivantichy.cz:10002/createserver" -d @server.json
-#VPN createServer
-curl -v -X PUT "http://digitalocean.ivantichy.cz:10002/createserver" -d @server.json
+#VPN createsubvpn 
+curl -v -X PUT "http://127.0.0.1:10002/createsubvpn" -d @ca.json
 
 #VPN createProfile
-curl -v -X PUT "http://digitalocean.ivantichy.cz:10002/createprofile" -d @profile.json
-#VPN createProfile
-curl -v -X PUT "http://digitalocean.ivantichy.cz:10002/createprofile" -d @profile.json
+curl -v -X PUT "http://127.0.0.1:10002/createprofile" -d @profile.json
 
 
 
-#CERT deleteCa
-curl -v -X DELETE "http://127.0.0.1:10001/deleteca?subvpn_name=tap-advanced-12345&subvpn_type=tap-advanced"
 
 pkill -e -f ".*cz.ivantichy.httpapi.handlers.vpnapi.*"
 
