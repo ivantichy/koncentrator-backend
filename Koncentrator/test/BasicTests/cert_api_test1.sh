218bf8323745037ea6de4d7ec7dce877a87a69c8
ivan <ivan.tichy@gmail.com>
2015-05-03 18:13:43 +0200

    autocommit

--- Koncentrator/test/BasicTests/cert_api_test1.sh
+++ Koncentrator/test/BasicTests/cert_api_test1.sh
@@ -13,6 +13,6 @@ java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.res
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest test tap-advanced-12345 tap-advanced
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.DeleteCaAdapterTest tap-advanced-12345 tap-advanced
 
-java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest test tun-basic-12345 tun-basic 
+java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest testx tun-basic-12345 tun-basic 
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest test2 tun-basic-12345 tun-basic
 java -classpath "Koncentrator/*:Koncentrator/lib/*" cz.ivantichy.supersimple.restapi.handlers.GenerateProfileAdapterTest test tun-basic-12345 tun-basic
